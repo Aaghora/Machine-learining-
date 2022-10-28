db689e5b3f0e61a513871e7168ba42e09810e0a4
Aaghora <kirandeoyadav@gmail.com>
2022-10-28 15:10:04 +0530

    Updated

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app= Flask(__name__)
 @app.route("/",methods=['GET','POST'])
 
 def index():
-    return "Starting_Project"
+    return "CI / CD Pipelines has been successfully established"
 
 if __name__=="__main__":
     app.run(debug=True)
